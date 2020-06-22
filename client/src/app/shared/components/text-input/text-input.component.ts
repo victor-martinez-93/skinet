959580afd9de37823629c79c03a102f46cf985b2
Epsilon Prog. Net <epsilon.prgnet@grupocastilla.es>
2020-06-22 12:11:10 +0200

    End of section 17

---
+++ client/src/app/shared/components/text-input/text-input.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, ViewChild, ElementRef, Input, Self, AfterViewInit } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.scss']
+})
+export class TextInputComponent implements OnInit, ControlValueAccessor {
+  @ViewChild('input') input: ElementRef;
+  @Input() type = 'text';
+  @Input() label: string;
+
+  constructor(@Self() public controlDir: NgControl) {
+    this.controlDir.valueAccessor = this;
+  }
+
+  ngOnInit() {
+    const control = this.controlDir.control;
+    const validators = control.validator ? [control.validator] : [];
+    const asyncValidators = control.asyncValidator ? [control.asyncValidator] : [];
+
+    control.setValidators(validators);
+    control.setAsyncValidators(asyncValidators);
+    control.updateValueAndValidity();
+  }
+
+  onChange(event) {
+
+  }
+
+  onTouched() {}
+
+  writeValue(obj: any): void {
+    if (this.input) {
+      this.input.nativeElement.value = obj || '';
+    }
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+}
